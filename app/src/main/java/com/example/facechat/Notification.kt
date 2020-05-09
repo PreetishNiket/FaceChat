96e7e7641f74db660c7d8e9336b94d7e9ce59f8f
Preetish <niketpreetish@gmail.com>
2020-05-09 19:35:42 +0530

    Initial Commit2

--- app/src/main/java/com/example/facechat/Notification.kt
+++ app/src/main/java/com/example/facechat/Notification.kt
@@ -2,11 +2,29 @@ package com.example.facechat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.find_friends_design.view.*
 
 class Notification : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_notification)
+    }
+    class FindFriendsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun FindFriendsViewHolder(itemView: View)
+        {
+            val userNameTxt=itemView.findViewById<TextView>(R.id.name_notification)
+            val acceptBtn=itemView.findViewById<Button>(R.id.request_accept_btn)
+            val cancelBtn=itemView.findViewById<Button>(R.id.request_decline_btn)
+            val profileImageView=itemView.findViewById<ImageView>(R.id.image_notification)
+            val cardView=itemView.findViewById<CardView>(R.id.card_view)
+        }
+
     }
 }
