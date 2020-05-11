3b342984e07597fd443bc32423cd5e9325e686cd
Preetish <niketpreetish@gmail.com>
2020-05-12 00:23:37 +0530

    Code Changes

--- app/src/main/java/com/example/facechat/SettingsActivity.kt
+++ app/src/main/java/com/example/facechat/SettingsActivity.kt
@@ -10,9 +10,13 @@ import android.widget.Toast
 import com.google.android.gms.tasks.Continuation
 import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.UploadTask
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_settings.*
 import java.util.*
 import kotlin.collections.HashMap
@@ -44,52 +48,66 @@ class SettingsActivity : AppCompatActivity() {
         save_settings_btn.setOnClickListener {
             val getUserName=username_settings.text.toString()
             val getUserStatus=bio_settings.text.toString()
+            //settings_profile_image.drawable.constantState==resources.getDrawable(R.drawable.profile_image).constantState
+            when {
+                imageUri==null -> {
+                    db.addValueEventListener(object :ValueEventListener{
+                        override fun onCancelled(dataSnapshot: DatabaseError) {
+                        }
+                        override fun onDataChange(dataSnapshot: DataSnapshot) {
+                           if (dataSnapshot.child(FirebaseAuth.getInstance().currentUser!!.uid).hasChild("image"))
+                           {
+                               savInfoOnlyWithoutImage()
+                           }
+                            else{
+                               Toast.makeText(this@SettingsActivity,"Please Select Profile Photo",Toast.LENGTH_SHORT).show()
+                           }
+                        }
 
-            if(settings_profile_image.drawable.constantState==resources.getDrawable(R.drawable.profile_image).constantState)
-            {
-
-            }
-            else if (getUserName == "")
-            {
-                username_settings.error="UserName Is Mandatory"
-            }
-            else if (getUserStatus=="")
-            {
-                bio_settings.error="Bio is Mandatory"
-            }
-            else{
+                    })
+                }
+                getUserName == "" -> {
+                    username_settings.error="UserName Is Mandatory"
+                }
+                getUserStatus=="" -> {
+                    bio_settings.error="Bio is Mandatory"
+                }
+                else -> {
                     val filepath=userProfileImgRef.child(FirebaseAuth.getInstance().currentUser!!.uid)
-                val uploadTask=filepath.putFile(imageUri!!)
-                uploadTask.continueWithTask(object :Continuation<UploadTask.TaskSnapshot, Task<Uri>>{
-                    override fun then(task: Task<UploadTask.TaskSnapshot>): Task<Uri> {
-                        if (!task.isSuccessful)
-                        {
-                            task.exception?.let {
-                                throw it
+                    val uploadTask=filepath.putFile(imageUri!!)
+
+                    uploadTask.continueWithTask(object :Continuation<UploadTask.TaskSnapshot, Task<Uri>>{
+                        override fun then(task: Task<UploadTask.TaskSnapshot>): Task<Uri> {
+                            if (!task.isSuccessful) {
+                                task.exception?.let {
+                                    throw it
+                                }
+                            }
+                            return filepath.downloadUrl
+                        }
+
+                    }).addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            val downloadUrl=it.result.toString()
+                            val profileMap=HashMap<String,String>()
+                            profileMap["uid"] = FirebaseAuth.getInstance().currentUser!!.uid
+                            profileMap["name"] = getUserName
+                            profileMap["status"]=getUserStatus
+                            profileMap["image"]=downloadUrl
+
+                            db.child(FirebaseAuth.getInstance().currentUser!!.uid).setValue(profileMap).addOnCompleteListener { task->
+                                if (task.isSuccessful) {
+                                    startActivity(Intent(this,ContactsActivity::class.java))
+                                    Toast.makeText(this,"Settings Updated",Toast.LENGTH_SHORT).show()
+                                }
                             }
                         }
-                        return filepath.downloadUrl
-                    }
-
-                }).addOnCompleteListener {
-                    if (it.isSuccessful)
-                    {
-                        val downloadUrl=it.result.toString()
-                        val profileMap=HashMap<String,String>()
-                        profileMap["uid"] = FirebaseAuth.getInstance().currentUser!!.uid
-                        profileMap["name"] = getUserName
-                        profileMap["status"]=getUserStatus
-                        profileMap["image"]=downloadUrl
-
-                        db.child(FirebaseAuth.getInstance().currentUser!!.uid).updateChildren(
-                            profileMap as Map<String, Any>
-                        )
                     }
                 }
             }
         }
+        retrieveUserInfo()
     }
-
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode==galleryPick &&requestCode== Activity.RESULT_OK&&data!=null)
@@ -97,5 +115,47 @@ class SettingsActivity : AppCompatActivity() {
             imageUri=data.data
             settings_profile_image.setImageURI(imageUri)
         }
+    }
+    private fun savInfoOnlyWithoutImage() {
+        val getUserName=username_settings.text.toString()
+        val getUserStatus=bio_settings.text.toString()
+        if (getUserName == "")
+        {
+            username_settings.error="UserName Is Mandatory"
+        }
+        else if (getUserStatus=="")
+        {
+            bio_settings.error="Bio is Mandatory"
+        }
+        else{
+            //Progress Dialog video6 26:30
+            val profileMap=HashMap<String,String>()
+            profileMap["uid"] = FirebaseAuth.getInstance().currentUser!!.uid
+            profileMap["name"] = getUserName
+            profileMap["status"]=getUserStatus
+            db.child(FirebaseAuth.getInstance().currentUser!!.uid).setValue(profileMap).addOnCompleteListener { task->
+                if (task.isSuccessful) {
+                    startActivity(Intent(this,ContactsActivity::class.java))
+                    Toast.makeText(this,"Settings Updated",Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+    private fun retrieveUserInfo(){
+        db.child(FirebaseAuth.getInstance().currentUser!!.uid).addValueEventListener(object :ValueEventListener{
+            override fun onCancelled(p0: DatabaseError) {
+            }
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                if (dataSnapshot.exists())
+                {
+                    val imageDb=dataSnapshot.child("image").value.toString()
+                    val nameDb=dataSnapshot.child("name").value.toString()
+                    val bioDb=dataSnapshot.child("status").value.toString()
+                    username_settings.setText(nameDb)
+                    bio_settings.setText(bioDb)
+                    Picasso.get().load(imageDb).placeholder(R.drawable.profile_image).into(settings_profile_image)
+                }
+            }
+        })
     }
 }
